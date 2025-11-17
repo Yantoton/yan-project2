c77f6590ad0d45bdc95ed56e7bf9abde58994f9a
Yan Toton <develroman320@gmail.com>
2025-11-17 23:06:16 +0600

    Add personal introduction script in About Mahiyan program

---
+++ 1.(About Mahiyan program).py
@@ -0,0 +1,8 @@
+name ="Mahiyan"
+age =30
+work ="Farmin & Coding"
+goals ="software engineer & Farming businessman"
+gmail ="develroman320@gmail.com"
+number = "01603532034"
+detail =f"Hi I am \"{name}\"\nI am {age} years old \nMy work is  \"{work}And\"\nmy goals are simple i want to be a\n\'{goals}\'\nIf you like to work with me then contact me\'{number}\'\nhere is my \'{gmail}\' gamil \n  Thank you so much "
+print(detail.upper())
