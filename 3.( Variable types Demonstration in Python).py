2386cc057093c09c53f470131abcfc3ba508187f
Yan Toton <develroman320@gmail.com>
2025-11-21 00:54:30 +0600

    Add variable types demonstration in Python

---
+++ 3.( Variable types Demonstration in Python).py
@@ -0,0 +1,45 @@
+
+# INTEGER
+adress = 153/1  # THIS IS A INTEGER VARIABLE
+have = 2 # THIS IS A INTEGER VARIABLE
+f = 1 # THIS IS A INTEGER VARIABLE
+print(f"My home adress is:{adress}") #PRINTING INTEGER VALUE
+print(f"you have \'{have}\' phone")#PRINTING INTEGER VALUE
+print(f"F{f} car reacing") #PRINTING INTEGER VALUE
+
+# FLOAT
+price =10.99# THIS IS A FLOAT VARIABLE
+gpa = 4.5# THIS IS A FLOAT VARIABLE
+kg = 1.5# THIS IS A FLOAT VARIABLE
+print(f"Fortnite battle pass is {price} only")#PRINTING FLOAT VALUE
+print(f"My gpa is {gpa}")#PRINTING FLOAT VALUE
+print(f"Chicken wight is {kg}")#PRINTING FLOAT VALUE
+
+#STRING
+name ="yan"# THIS IS A STRING VARIABLE
+email = "loser2984@gmail.com"# THIS IS A STRING VARIABLE
+car ="toyota gt 86"# THIS IS A STRING VARIABLE
+
+print(f"hy \'{name}\' what? is yor email \'{email}\' ")#YOU CAN ALSO PRINT 2_VARIABLE STRING IN 1 LINE
+print(f"My dream car is \'{car}\'")#PRINTING STRING VALUE
+
+#BOOLEAN
+online =True# THIS IS A BOOLEAN VARIABLE
+play = False# THIS IS A BOOLEAN VARIABLE
+learnig = True# THIS IS A BOOLEAN VARIABLE
+
+print(f"Are u online? {online}")#PRINTING  BOOLEAN VALUE
+print(f"Do u quit playing fortnite {play}")#PRINTING BOOLEAN VALUE
+print(f"Did u learning python {learnig}")#PRINTING  BOOLEAN VALUE
+
+#MANY VALUS TO MULTIPLE VARIABLES
+a ,f , g = "Hello",False,1.99# THIS IS A MULTIPLE VARIABLE
+print(a)#PRINTING MULTIPLE VARIABLE VALUE
+print(f)#PRINTING MULTIPLE VARIABLE VALUE
+print(g)#PRINTING MULTIPLE VARIABLE VALUE
+
+#ONE VALUE TO MULTIPLE VARIABLES
+h = s = z ="ps4",True,34.5 #THIS IS A MULTIPLE VARIABLE
+print(h)#PRINTING MULTIPLE VARIABLE VALUE
+print(s)#PRINTING MULTIPLE VARIABLE VALUE
+print(z)#PRINTING MULTIPLE VARIABLE VALUE
