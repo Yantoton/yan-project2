f1a137cc70cc23ed9a5e05c8e7248bab9fb9a587
Yan Toton <develroman320@gmail.com>
2025-12-22 02:32:00 +0600

    Add program to collect favorite programming languages

---
+++ program5.(program to collect favorite programming languages).py
@@ -0,0 +1,6 @@
+prog_language=[]
+prog_language.append(input("Enter your 1st favorite programming language:"))
+prog_language.append(input("Enter your 2nd favorite programming language:"))
+prog_language.append(input("Enter your 3rd favorite programming language:"))
+
+print(prog_language)
