e6e344ee96201be007ebc9b525370488c8143bd3
Yan Toton <develroman320@gmail.com>
2025-12-22 21:13:40 +0600

    Add list comparison and reversal logic

---
+++ program-6.( list comparison and reversal logic).py
@@ -0,0 +1,8 @@
+list1=['m','a','m','a']
+
+copy_list1=list1.copy()
+copy_list1.reverse()
+if copy_list1==list1:
+    print("the same value in this list")
+else:
+    print(" not the value same in this list")
