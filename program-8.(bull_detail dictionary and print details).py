ad3b9981967cf19225525bc65033b46939f9f0da
Yan Toton <develroman320@gmail.com>
2025-12-27 18:28:23 +0600

    Add bull_detail dictionary and print details

---
+++ program-8.(bull_detail dictionary and print details).py
@@ -0,0 +1,16 @@
+bull_detail ={
+    "Name": "Sikandar",
+    "Breed":"Shahiwal",
+    "Birth_date":"11-february-2023",
+    "Id number":576,
+    "Teeth":2,
+    "Age":"2_year",
+    "Wight":"512_kg",
+    "Hight":"60_inch",
+    "Fatting": True,
+    "Vaccinated": True,
+    "Any disise":False
+
+}
+for key,value in bull_detail.items():
+    print(f"{key}:{value}")
