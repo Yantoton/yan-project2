d2e1f43085249fdcfe574531f4fc16a64fcb5b4f
Yan Toton <develroman320@gmail.com>
2026-01-03 22:13:16 +0600

    Add mobile list check for None values

---
+++ program-15.().py
@@ -0,0 +1,7 @@
+mobile =['i phone','samsung',None,'redme']
+for name in mobile:
+    if name is None:
+     print("we didn't find any missing mobile's ?...")
+     break
+else:
+    print("we found some missing mobile's !...",mobile)
