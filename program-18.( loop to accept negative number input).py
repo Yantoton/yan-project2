b2f5cfb5f2cc7757b0bc4caba69404c985014e5c
Yan Toton <develroman320@gmail.com>
2026-01-08 01:17:40 +0600

    Implement loop to accept negative number input

---
+++ program-18.( loop to accept negative number input).py
@@ -0,0 +1,9 @@
+
+while True:
+    num = int(input("Enter a negative number:"))
+    if num < 0:
+        print(f"this is a negative number:{num}")
+        break
+    else:
+        pass
+        print(f"this is not a negative number:{num}")
