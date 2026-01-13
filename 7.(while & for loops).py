616ed5228e959aad66d495c538c6bfe58a801ce5
Yan Toton <develroman320@gmail.com>
2026-01-14 03:29:00 +0600

    Create 7.(while & for loops).py

---
+++ 7.(while & for loops).py
@@ -0,0 +1,11 @@
+count = 0
+while count <= 10:
+    print(count)
+    count += 2
+  
+  for x in range (10):
+    print("10 x",x,"=",10 * x )
+    if x == 10:
+        break
+        # continue
+print("Loop end here!...")
