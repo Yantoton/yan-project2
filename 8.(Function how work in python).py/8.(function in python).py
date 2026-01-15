ee6877b09fd59446eb8c0ab13f2ba3537ccdcfc5
Yan Toton <develroman320@gmail.com>
2026-01-16 02:24:49 +0600

    Create 8.(function in python).py
    এটি পাইথনের একটি ফাংশন এবং সর্বাধিক পোরগ্রামার এটি ব্যবহার করে

---
+++ 8.(Function how work in python).py/8.(function in python).py
@@ -0,0 +1,10 @@
+
+def mc(first_name,last_name,):
+    print(f"Hello {first_name} the lord of Darkness {last_name}")
+    return "Welcome to Dark_$ide"
+talk = mc("Yan","return")
+file = open("content.txt","w")
+
+file.write(talk)
+
+mc("mahiyan","MYT")
