5007859d1c06e2a116caa493fe02dedf92d4a82e
Yan Toton <develroman320@gmail.com>
2026-01-17 23:58:41 +0600

    Create program-_-27(Function input Even Odd checker).py

---
+++ program-_-27(Function input Even Odd checker).py
@@ -0,0 +1,8 @@
+def even_odd():
+    number = int(input("Enter number here!...:"))
+    if number % 2 == 0:
+        print(f"This is an even number {number}")
+    else:
+        print(f"This is an odd number {number}")
+print()
+even_odd()
