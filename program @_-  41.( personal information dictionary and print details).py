895bba24b19883b2c6694f3803fbd25b92852fcd
Yan Toton <develroman320@gmail.com>
2026-02-01 03:44:49 +0600

    Create personal information dictionary and print details
    Added a personal information dictionary and printed details.
    This program stores the personal data of 1 person.
    
    একটি ব্যক্তিগত তথ্য অভিধান এবং মুদ্রিত বিবরণ যোগ করা হয়েছে।
    
    এই প্রোগ্রামটি ১ জনের ব্যক্তিগত তথ্য সংরক্ষণ করে।

---
+++ program @_-  41.( personal information dictionary and print details).py
@@ -0,0 +1,17 @@
+this_dict = {
+    "Name":"Y_a_n(@_-)T_Ton",
+    "Age": 28,
+    "City": "dhaka",
+    "Address":"153/1 Hamid Tower south mugda dhaka 1214 Bangladesh"
+}
+this_dict["Programmer"] = "yes"
+this_dict["Github"] = "https://github.com/Yantoton"
+this_dict["Email"] = "develroman320@gmail.com"
+this_dict["Facebook"] = "https://www.facebook.com/roman.ishan.1"
+this_dict["Instagram"] = "https://www.instagram.com/yan_toton_/"
+this_dict["WhatsApp"] = "01603532034"
+
+for keys,values in this_dict.items():
+    print(keys,"<:>",values)
+else:
+    print("program run successfully")
