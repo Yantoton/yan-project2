ca97d2d2c819bf811f0ff1c053405f225333c161
Yan Toton <develroman320@gmail.com>
2026-02-02 02:04:04 +0600

    Add list entry to python_basic dictionary
    this way u can add keys & value in dictionary
    
    এইভাবে আপনি   dictionary  (key) এবং (value) যোগ করতে পারেন

---
+++ 12,Dictionary & All method .py/3.add method in dictionay.py
@@ -0,0 +1,13 @@
+
+python_basic = {
+    "variables" : "2,hello,2.0",
+    "data_type" : "str,int,float,complex,bool",
+    "conditionals" : "if,elif,else",
+    "loops" : "for,while",
+    "logicals" : "and,or,not",
+    "operators" : "+,-,*,/,<,>,==,!=,<=,>=",
+    " function" : "def():"
+
+}
+python_basic ["list"] = "[,,,]"
+print(python_basic)
