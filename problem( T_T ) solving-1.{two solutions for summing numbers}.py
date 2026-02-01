2734e98b9872ee794664659002374da7535af5fb
Yan Toton <develroman320@gmail.com>
2026-02-01 22:47:00 +0600

    Add two solutions for summing numbers in Python
    try that way to solving Problem / Added two solutions for summing numbers: one using user input and another with predefined variables.
    
    সমস্যা সমাধানের জন্য এইভাবে চেষ্টা করুন / সংখ্যার যোগফলের জন্য দুটি সমাধান যোগ করা হয়েছে: একটি ব্যবহারকারীর ইনপুট ব্যবহার করে এবং অন্যটি পূর্বনির্ধারিত ভেরিয়েবল সহ।

---
+++ problem( T_T ) solving-1.{two solutions for summing numbers}.py
@@ -0,0 +1,16 @@
+#Solution-1 (with user_input- you use float also)
+
+
+num1 =int(input("Enter a number:"))
+
+num2 =int(input("Enter another number:"))
+
+print("the total sum of Two number:",num1+num2)
+
+
+#Solution-2 (with pre-defined variables)
+
+num1 = 12
+num2 = 14
+sum = num1 + num2
+print("The total sum of Two number:",sum)
