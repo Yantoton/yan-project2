d34024cf549cfea2d9140d05b7bed9f2386cc4dc
Yan Toton <develroman320@gmail.com>
2026-02-03 03:05:39 +0600

    Implement dictionary for Python basics with get method
    Using the third bracket [] directly will result in an error if the key does not exist, but using get() will not result in an error.
    
    সরাসরি থার্ড ব্র্যাকেট [] ব্যবহার করলে কী (Key) না থাকলে এরর আসে, কিন্তু get() ব্যবহার করলে এরর আসে না।

---
+++ 12,Dictionary & All method .py/9.get merhod in dictionary.py
@@ -0,0 +1,11 @@
+python_basic = {
+    "variables" : "2,hello,2.0",
+    "data_type" : "str,int,float,complex,bool",
+    "conditionals" : "if,elif,else",
+    "loops" : "for,while",
+    "logicals" : "and,or,not",
+    "operators" : "+,-,*,/,<,>,==,!=,<=,>=",
+    " function" : "def():"
+
+}
+print(python_basic.get("variables")) #Finding Value
