cd6aa334be2649455bed4f6f961384e194c7b117
Yan Toton <develroman320@gmail.com>
2026-02-03 02:00:12 +0600

    Add python_basic dictionary and demonstrate pop methods
    data deletion
    
     তথ্য মোছা

---
+++ 12,Dictionary & All method .py/5.pop () & popitem() in dictionary.py
@@ -0,0 +1,15 @@
+python_basic = {
+    "variables" : "2,hello,2.0",
+    "data_type" : "str,int,float,complex,bool",
+    "conditionals" : "if,elif,else",
+    "loops" : "for,while",
+    "logicals" : "and,or,not",
+    "operators" : "+,-,*,/,<,>,==,!=,<=,>=",
+    " function" : "def():"
+
+}
+python_basic ["list"] = "[,,,]"
+
+python_basic.pop("list") # pop() this Deletes the specified key.
+
+python_basic.popitem() # popitem ()this Delete the last one key.
