d1b569ff4139d0541610d95ce1db85f81d9e9b0e
Yan Toton <develroman320@gmail.com>
2026-02-03 02:50:41 +0600

    Update python_basic dictionary with new data types
    It is used to update multiple pieces of information at once.
    
    একসাথে অনেকগুলো তথ্য আপডেট করতে এটি ব্যবহার করা হয়

---
+++ 12,Dictionary & All method .py/7.update method in dictionary.py
@@ -0,0 +1,12 @@
+python_basic = {
+    "variables" : "2,hello,2.0",
+    "data_type" : "str,int,float,complex,bool",
+    "conditionals" : "if,elif,else",
+    "loops" : "for,while",
+    "logicals" : "and,or,not",
+    "operators" : "+,-,*,/,<,>,==,!=,<=,>=",
+    " function" : "def():"
+
+}
+
+python_basic.update({"list":"[,,,]","tuple":"(,,,)","set":"{,,,}","dictionary":"{"":""}"}) #Add or change information
