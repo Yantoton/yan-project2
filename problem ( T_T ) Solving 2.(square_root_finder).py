b6182ca2f5d4b87751c48632741ed443ba75b123
Yan Toton <develroman320@gmail.com>
2026-02-05 02:38:44 +0600

    Create problem ( T_T ) Solving 2.(square_root_finder python-math-calculators).py

---
+++ problem ( T_T ) Solving 2.(square_root_finder).py
@@ -0,0 +1,11 @@
+# Solution-1 (using Exponention)
+number = 86
+num= int(input("Enter a number:"))
+sqr = num **(1/2)
+print("the square root of the user number is:",sqr)
+
+# Solution-2 (using Math Module)
+import math
+num = int(input("Plz...Enter a number:.."))
+sqr = math.sqrt (num)
+print(f"the square root of {num} is {sqr}")
