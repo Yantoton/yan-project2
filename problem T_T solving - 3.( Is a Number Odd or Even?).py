e447e6e7c326604016871b5bea615d46fd11375c
Yan Toton <develroman320@gmail.com>
2026-02-12 02:58:00 +0600

    Update problem T_T solving - 3.( Is a Number Odd or Even?).py

--- problem T_T solving - 3.( Is a Number Odd or Even?).py
+++ problem T_T solving - 3.( Is a Number Odd or Even?).py
@@ -4,6 +4,6 @@ if num % 2 == 0:
     print("The number is even")
 else:
     print("The number is odd")
-# Sollution - 2 By (Short_Hand if else)
+# Sollution - 2 By (Short_Hand if else Statement)
 num = int(input("Enter the number:"))
 print("The number is even") if num % 2 == 0 else print("The number is odd")
