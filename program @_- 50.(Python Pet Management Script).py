218cc6b11ededebbc2826e0ac08cacc5652127c3
Yan Toton <develroman320@gmail.com>
2026-02-16 15:27:36 +0600

    Create program @_- 50.(Python Pet Management Script).py

---
+++ program @_- 50.(Python Pet Management Script).py
@@ -0,0 +1,6 @@
+pet_names = ["Dog","Cat","Monster_Fish_Tank","Bird_Colony"]
+
+for index,pet_name in enumerate(pet_names,start=1):
+    print(f"I have these pet:{index}-{pet_name}")
+    if pet_name == "Dog":
+        print("My dog name is Coco")
