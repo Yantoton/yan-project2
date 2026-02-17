e188dc57545920284acf58982575de3ea5cb61d3
Yan Toton <develroman320@gmail.com>
2026-02-17 13:13:17 +0600

    Create MAC FUT.py

---
+++ program2.()/MAC FUT.py
@@ -0,0 +1,14 @@
+# for i in range(0,8887):
+#     print(i)
+# nam = "Mahiyan ahmed chowdhury"
+#
+# for i in  nam:
+#     print(i)
+#     if i == "M":
+#         print(f"In Sha Allah 8886 crore tk r hodhikari {nam}")
+
+# for i in range (8):
+#     print("interation no {} in for loop".format(i+1))
+# else:
+#     print("else loop is active")
+# print("out of program")
