e08e262c5fe2898c04b6ae5ebaf65721f39628a0
Yan Toton <develroman320@gmail.com>
2026-02-25 02:02:53 +0600

    Add number positivity, negativity, and zero check

---
+++ problem T_T Solving (number check for positivity, negativity, and zero).py
@@ -0,0 +1,8 @@
+num_check = int(input("Let's check this number:"))
+
+if num_check >= 1:
+    print(num_check,"The entered number is positive.")
+elif num_check == 0:
+    print(num_check,"The entered number is zero.")
+else:
+    print(num_check,"The entered number is negative.")
